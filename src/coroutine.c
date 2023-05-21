68d9d27d16ef02c247e23886d6926a7303cacee3
niugx <niu91@qq.com>
2023-05-21 12:52:04 +0000

    x

--- src/coroutine.c
+++ src/coroutine.c
@@ -1,12 +1,77 @@
 #include "coroutine.h"
+
+#include <stdio.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+pthread_key_t global_sched_key;
+static pthread_once_t sched_key_once = PTHREAD_ONCE_INIT;
+
+static void sched_key_destructor(void *data) { free(data); }
+static void coroutine_sched_key_creator() {
+    assert(pthread_key_create(&global_sched_key, sched_key_destructor) == 0);
+    // 线程局部数据存储
+    assert(pthread_setspecific(global_sched_key, NULL) == 0);
+}
+
 // 协程的创建
-int coroutine_create() { return 0; }
+int coroutine_create(coroutine_t **new_co, func_t func, void *arg) {
+    // 在多线程环境中只执行一次
+    assert(pthread_once(&sched_key_once, coroutine_sched_key_creator) == 0);
+
+    schedule_t *sched = get_sched();
+    if (sched == NULL) {
+        schedule_create(0);  // 创建一个调度器
+        sched = get_sched();
+        if (sched == NULL) {
+            printf("failed get sched!\n");
+            return -1;
+        }
+    }
+
+    // 协程申请内存
+    coroutine_t *co = calloc(1, sizeof(coroutine_t));
+    co->stack = NULL;
+    co->stack_size = 0;
+    co->sched = sched;
+    co->status = 0;  // ready
+    co->func = func;
+    co->id = sched->corouties++;
+    co->fd = -1;
+    co->events = 0;
+
+    *new_co = co;
+    return 0;
+}
 
 // 调度器运行
 void schedule_run() {
     // 获取调度器
     schedule_t *sched = get_sched();
     if (sched == NULL) return;
+
+    while (1) {
+        // 休眠队列
+
+        // 就绪队列
+
+        // 等待队列
+    }
 }
 
-schedule_t *get_sched() {}
+schedule_t *get_sched() { return pthread_getspecific(global_sched_key); }
+
+int schedule_create(int stack_size) {
+    schedule_t *sched =
+        (schedule_t *)calloc(1, sizeof(schedule_t));  // 调度器分配内存
+    assert(pthread_setspecific(global_sched_key, sched) == 0);
+
+    // epoll来管理事件
+    sched->poller_fd = epoll_create(1024);
+    // 注册触发器
+    // 栈大小，默认64K
+    sched->stack_size = stack_size ? stack_size : (64 * 1024);
+    sched->page_size = getpagesize();
+    posix_memalign(&sched->stack, sched->page_size,
+                   sched->stack_size);  // 页内存申请
+    return 0;
+}
--- src/coroutine.h
+++ src/coroutine.h
@@ -1,13 +1,26 @@
 #if !defined(__COROUTINE_H__)
 #define __COROUTINE_H__
+#include <assert.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <ucontext.h>
+
+#include "queue.h"
 // 协程回调函数类型
 typedef void (*func_t)(void*);
 
 // 核心数据结构： 调度器
 typedef struct schedule_t {
     /* data */
+    ucontext_t ctx;
+    void* stack;
+    size_t stack_size;
+    int page_size;
+
+    // epoll
+    int poller_fd;
+
+    int corouties;
 } schedule_t;
 
 // 核心数据结构： 协程
@@ -17,13 +30,24 @@ typedef struct coroutine_t {
     func_t func;     // 回调函数
     void* arg;       // 回调函数参数
     int status;      // 运行状态
+
+    u_int64_t id;
+    int fd;
+    int events;
+
+    void* stack;
+    size_t stack_size;
+    schedule_t* sched;
+
 } coroutine_t;
 
 // 协程的创建
-int coroutine_create();
+int coroutine_create(coroutine_t** co, func_t func, void* arg);
 
 // 获取调度器
 schedule_t* get_sched();
+// 创建调度器
+int schedule_create(int stack_size);
 // 调度器运行
 void schedule_run();
 
